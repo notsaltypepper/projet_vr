ae2ae8989178497bcf09be140ea76656dddb82fc
notsaltypepper <88979389+notsaltypepper@users.noreply.github.com>
2023-02-01 10:03:48 +0100

    minor modifications

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private NavMeshAgent nma;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        nma.isStopped = false;
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        // Set destination
+        nma.SetDestination(player.transform.position);
+    }
+}
