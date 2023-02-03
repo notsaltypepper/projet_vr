4d984b259c41d8975af9e427023f3341b5592eea
alx9865 <alx9865@gmail.com>
2023-02-03 10:36:35 +0100

    second commit 2.2.23

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -32,11 +32,11 @@ public class Gun : MonoBehaviour
 
         bool hit = Physics.Raycast(firepoint.transform.position, firepoint.transform.forward, out hitInfo);
 
-        //if (hit)
-        //{
-        //    GameObject fx = Instantiate(hitFxPrefab, hitInfo.point, Quaternion.Euler(firepoint.transform.forward));
-        //    Destroy(fx, 5f);
-        //}
+        if (hit)
+        {
+            GameObject fx = Instantiate(hitFxPrefab, hitInfo.point, Quaternion.Euler(firepoint.transform.forward));
+            Destroy(fx, 5f);
+        }
 
         if (hit && hitInfo.collider.tag == "Destructible")
         {
